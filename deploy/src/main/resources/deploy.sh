2b7f51434717f1a21058e68982deecd27fe04210
Richard Vowles <richard@bluetrainsoftware.com>
2019-11-01 19:18:20 +1300

    remove cruft

--- deploy/src/main/resources/deploy.sh
+++ deploy/src/main/resources/deploy.sh
@@ -7,6 +7,6 @@ kubectl config set-context gce --namespace=${ENV}
 kubectl config get-contexts
 kubectl get secrets
 #kubectl config set-context `kubectl config current-context` --namespace=${ENV}
-echo helm upgrade featurehub featurehub -f featurehub/${ENV}-values.yaml --debug -i
+echo helm upgrade oguretsexample oguretsexample -f oguretsexample/${ENV}-values.yaml --debug -i
 # this is always causing it to go into pending-install instead of deployed
-helm upgrade featurehub featurehub -f featurehub/${ENV}-values.yaml -f manifest.yaml --debug -i
+helm upgrade oguretsexample oguretsexample -f oguretsexample/${ENV}-values.yaml -f manifest.yaml --debug -i
