1422ada71ebc5aed46d419c2b43f0e816d47b397
Irina Southwell <irina.southwell@clearpoint.co.nz>
2019-11-01 17:55:03 +1300

    update plugin versions

--- dart_e2e/lib/common.dart
+++ dart_e2e/lib/common.dart
@@ -1,19 +1,16 @@
 
-
 import 'package:openapi_dart_common/openapi.dart';
-import 'package:openapi_dart_common/openapi_cli_api.dart';
-
 import 'package:counterapi/api.dart';
 
 class Common {
-  final ApiClient _apiClient;
+  ApiClient _apiClient;
   CounterServiceApi counterServiceApi;
 
-  Common() :
-      _apiClient = ApiClient(
-        basePath: "http://localhost:8076",
-        deserializeDelegate: LocalApiClient(),
-        apiClientDelegate: CliApiClientDelegate()) {
-    counterServiceApi = CounterServiceApi(_apiClient);
+  Common() {
+    _apiClient = ApiClient(
+      basePath: "http://localhost:8076",
+      deserializeDelegate: LocalApiClient());
+      counterServiceApi = CounterServiceApi(_apiClient);
+    }
   }
-}
+
